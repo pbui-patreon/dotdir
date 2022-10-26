8301ec31fce71f3d457003587194b55ca1f460ee
pbui-patreon <pbui@patreon.com>
2022-10-26 09:02:48 -0700

    Add vim

--- install.sh
+++ install.sh
@@ -3,4 +3,13 @@
 set -exo pipefail
 
 sudo apt-get update
-sudo apt-get install -y tmux fzf vim
+sudo apt-get install -y tmux fzf vim silversearcher-ag
+
+# Add fzf bindings to shell
+echo 'source /usr/share/doc/fzf/examples/key-bindings.bash' >> ~/.bashrc
+
+# Clone my vim setup
+gh clone paydro/vim-config ~/.vim
+cd ~/.vim
+git submodule update --init
+cd -
